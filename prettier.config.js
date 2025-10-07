692271a2bd0aaca479a8c05adc1246ec70b297b0
冰冻大西瓜 <1003582810@qq.com>
2025-10-07 14:21:20 +0800

    :wrench: prettier:添加格式化配置文件

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  htmlWhitespaceSensitivity: "ignore",
+  bracketSameLine: true,
+};
+
+export default config;
